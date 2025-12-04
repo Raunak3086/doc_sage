aec09f399cf65bb9edf589592de61099bb993a89
Raunak3086 <deepraunak45@gmail.com>
2025-12-04 16:38:11 +0530

    server starting

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: './id.env' });
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello from the backend!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
